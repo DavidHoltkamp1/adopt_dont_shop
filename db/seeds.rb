0e1b873f4861761a2db15449bf6b7d8214c6d700
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-29 14:46:20 -0700

    Seed two shelters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+shelter1 = Shelter.create(name: "Mike's Shelter", address: "1331 17th Street", city: "Denver", state: "CO", zip: "80202")
+shelter2 = Shelter.create(name: "Meg's Shelter", address: "150 Main St", city: "Hershey", state: "PA", zip: "17033")
+
+
+# There should be two pets as well, Mike's Shelter has one pet, Athena, 1 year old, female, description: butthead. Meg's Shelter has Odell, 4 years old, M and description: good dog.
 #
-# Examples:
+# You MUST reset and reseed the Heroku database before each time you run this spec harness.
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# You can run all tests by typing in rspec or you can run them individually by typing in, rspec <filename>
