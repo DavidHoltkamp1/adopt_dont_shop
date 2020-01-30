869147dc5a04addb72c59abbc35239a170dffb05
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-30 14:46:09 -0700

    Adjust delete spec to fix errors from creating shelters

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -1,15 +1,12 @@
+require 'rails_helper'
+
 RSpec.describe "shelter show page", type: :feature do
-  before do
+
+  it "clicks on delete shelter link" do
     @shelter1 = Shelter.create(name: "Mike's Shelter", address: "1331 17th Street", city: "Denver", state: "CO", zip: "80202")
     @shelter2 = Shelter.create(name: "Meg's Shelter", address: "150 Main St", city: "Hershey", state: "PA", zip: "17033")
-  end
 
-  it "sees a link to delete shelter" do
-    visit "/shelters/#{@shelter1.id}"
 
-    expect(page).to have_link("Delete Shelter")
-  end
-  it "clicks on delete shelter link" do
     visit "/shelters/#{@shelter1.id}"
 
     click_on "Delete Shelter"
