ae3e6b2712f81dab18a070c2a6e4eb7e52496c96
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-01 14:14:07 -0700

    Routing for pet show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
   get '/pets', to: 'pets#index'
   get '/shelters/:id/pets', to: 'shelterpets#index'
+  get '/pets/:id', to: 'pets#show'
 end
